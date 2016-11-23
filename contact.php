1371731426ea621ff6ac99c152789f0743ab0f38
tatain1 <tatain1@hotmail.com>
2016-11-23 10:53:37 +0100

    commit de 10h53

--- contact.php
+++ contact.php
@@ -1,5 +1,12 @@
-<?php
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+      <meta charset="UTF-8">
+      <title>Nom du projet</title>
+      <meta name="auteur" content="Kilian Mirail">
+      <meta name="descrip" content="Tuto Login">
+      <link rel="stylesheet" href="./css/bootstrap.css">
+      <link rel="stylesheet" href="./css/style.css">
+  </head>
 
-echo 'Salut les ptits clous';
-
- ?>
+</html>
