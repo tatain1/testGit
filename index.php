768866657975290e0d7a4322273ac649ff1e2ec2
tatain1 <tatain1@hotmail.com>
2018-02-15 21:09:10 +0100

    Add tabABC

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+$tabABC = array('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+                'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z');
+
+foreach ($tabABC as $dataABC) {
+  echo $dataABC. '<br>';
+}
+
+
+
+
+ ?>
