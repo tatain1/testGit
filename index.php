3c8da0efbb2e3ff8cf6b13ab3d601ee9f56b1c2f
tatain1 <tatain1@hotmail.com>
2018-02-15 21:20:49 +0100

    Suite

--- index.php
+++ index.php
@@ -6,7 +6,13 @@ foreach ($tabABC as $dataABC) {
   echo $dataABC. '<br>';
 }
 
+$tab9000 = array();
+for ($i=0; $i < 10; $i++) {
+  array_push($tab9000, $i);
+}
 
-
+echo '<pre>';
+print_r($tab9000);
+echo '</pre>';
 
  ?>
