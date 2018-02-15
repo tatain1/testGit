09f96665e240a92ad1ce9a99cd62a85c8eee1be4
tatain1 <tatain1@hotmail.com>
2018-02-15 21:03:38 +0100

    Initial comm

---
+++ contact.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+      <meta charset="UTF-8">
+      <title>Nom du projet</title>
+      <meta name="auteur" content="Kilian Mirail">
+      <meta name="descrip" content="Tuto Login">
+      <link rel="stylesheet" href="./css/bootstrap.css">
+      <link rel="stylesheet" href="./css/style.css">
+  </head>
+
+<<<<<<< HEAD
+</html>
