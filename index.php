834b1c7b3e3370878b85e0a9192d24694f1f79e0
tatain1 <tatain1@hotmail.com>
2018-02-15 21:58:19 +0100

    Fin du test tabABC + tab9000

--- index.php
+++ index.php
@@ -1,18 +1,54 @@
 <?php
+function debug($array, $die = 0) {
+  echo '<pre>';
+  print_r($array);
+  echo '</pre>';
+
+  if ($die == 1) {
+    die();
+  }
+}
+
+function slugify($text)
+{
+  $text = preg_replace('~[^\pL\d]+~u', '-', $text);
+  $text = iconv('utf-8', 'us-ascii//TRANSLIT', $text);
+  $text = preg_replace('~[^-\w]+~', '', $text);
+  $text = trim($text, '-');
+  $text = preg_replace('~-+~', '-', $text);
+  $text = strtolower($text);
+
+  if (empty($text)) {
+    return 'n-a';
+  }
+
+  return $text;
+}
+
+
+
+
 $tabABC = array('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
                 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z');
 
-foreach ($tabABC as $dataABC) {
-  echo $dataABC. '<br>';
-}
-
 $tab9000 = array();
-for ($i=0; $i < 10; $i++) {
+for ($i=1; $i < 9701; $i++) {
+  // $name = $i. '\''. $i;
   array_push($tab9000, $i);
 }
 
-echo '<pre>';
-print_r($tab9000);
-echo '</pre>';
+
+foreach ($tabABC as $dataABC) {
+  echo $dataABC. ' <input list="descrip" type="text">
+        <datalist id="descrip">';
+  foreach ($tab9000 as $data9000) {
+    $genFuck = $data9000.'é ë'.$data9000;
+    $genFuck = slugify($genFuck);
+    echo '<option value="'.$data9000.'a">'.$genFuck.'</option>';
+  }
+  echo '</datalist><br>';
+   // print_r($tab9000);
+}
+
 
  ?>
--- readme.md
+++ readme.md
@@ -1,2 +1,2 @@
-test 1
-test pour le second commit
+Test divers
+Ne faites pas trop attention, c'est majoritairement inutile, mais ça me permet de recuperer des bouts de codes d'un endroit à un autre sans les copier sur un support USB.
